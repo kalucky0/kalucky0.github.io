ef3b12964efc82ef38d14d7a4a09e5fd6d8316a9
kalucky0 <kalucki23@gmail.com>
2018-03-19 17:55:14 +0100

    Update youtube.js

--- inc/youtube.js
+++ inc/youtube.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDeYhVMKOdmVzoBrsjvf2JJukLCiJO5ACg",
+    authDomain: "hazard-bm.firebaseapp.com",
+    databaseURL: "https://hazard-bm.firebaseio.com",
+    projectId: "hazard-bm",
+    storageBucket: "hazard-bm.appspot.com",
+    messagingSenderId: "805444506237"
+  };
+  firebase.initializeApp(config);
